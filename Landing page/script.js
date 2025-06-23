ded18b75b3c9558aba10c7c1ac7c97f65437aba0
Harsh Raj <harsh120905@gmail.com>
2025-06-23 22:00:41 +0530

    Add files via upload

---
+++ Landing page/script.js
@@ -0,0 +1,39 @@
+function toggleMenu(){
+    console.log("toggleMenu() called");
+    const menu = document.querySelector('.menu');
+    const nav = document.querySelector('.nav');
+    menu.classList.toggle('active');
+    nav.classList.toggle('active');
+}
+
+
+function changeVideo(name){
+    const bgVideoList = document.querySelectorAll('.bg-video');
+    const trailers = document.querySelectorAll('.trailer');
+    const models = document.querySelectorAll('.model');
+
+    bgVideoList.forEach(video =>{
+        video.classList.remove('active');
+        if(video.classList.contains(name)){
+            video.classList.add('active')
+        }
+    });
+
+
+models.forEach(model =>{
+    model.classList.remove('active');
+    if(model.classList.contains(name)){
+        model.classList.add('active')
+    }
+});
+
+
+trailers.forEach(trailer =>{
+    trailer.classList.remove('active');
+    if(trailer.classList.contains(name)){
+        trailer.classList.add('active')
+    }
+});
+
+}
+
